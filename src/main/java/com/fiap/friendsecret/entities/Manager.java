8e10bfed2e61dac500c0d80c688c1a497d77472a
Wagner Carvalho <wagner.carvalho@rappi.com>
2019-11-23 18:05:39 -0300

    Hot fix array to questionBot

--- src/main/java/com/fiap/friendsecret/entities/Manager.java
+++ src/main/java/com/fiap/friendsecret/entities/Manager.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class Manager {
 
     static Map<String, Object> response = new HashMap<>();
-    static String questionBot;
+    static HashMap<Integer, String> questionBot = new HashMap<>();
     static Person user = new Person();
     private String answerUser;
     private User owner;
@@ -26,43 +26,58 @@ public class Manager {
     }
 
     public String checkMessage(String welcome) {
-        return questionBot == null? greeting(welcome): parseResult(response.get(questionBot), answerUser);
-    }
-
-    private String parseResult(Object options, String answerUser) {
-        switch (((JSONArray) options).size()) {
-            case 0:
-                return notFound();
-            case 1:
-                return flowSimple(options);
-            default:
-                return flowMultiple(options);
+        //simplificar IF
+        String result;
+        if (questionBot.size() == 0) {
+            result = greeting(welcome);
+        } else if (questionBot.containsKey(owner.id())) {
+            result = parseResult(response.get(questionBot.get(owner.id())), answerUser, welcome);
+        } else {
+            result = greeting(welcome);
         }
+        return result;
     }
 
-    private String greeting(String welcome) {
-        user.setUser(owner);
-        answerUser = null;
-        questionBot = welcome;
-        return "Olá, " + user.getFirstName() + "! " + questionBot;
-    }
-
-    private String flowSimple(Object options) {
-        questionBot = ((JSONArray) options).get(0).toString();
-        return user.getFirstName() + ", " + answerUser + "!!! " + questionBot;
-    }
-
-    private String notFound() {
-        return questionBot = "Desculpe, não entendi sua resposta";
+    private String parseResult(Object options, String answerUser, String welcome) {
+        try {
+            switch (((JSONArray) options).size()) {
+                case 0:
+                    return notFound();
+                case 1:
+                    return flowSimple(options);
+                default:
+                    return flowMultiple(options);
+            }
+        } catch (Exception e) {
+            return greeting(welcome);
+        }
     }
 
     private String flowMultiple(Object options) {
         ((JSONArray) options).forEach(item -> {
             if (((JSONObject) item).containsKey(answerUser.toUpperCase())) {
-                questionBot = ((JSONObject) item).get(answerUser.toUpperCase()).toString();
+                questionBot.put(owner.id(), ((JSONObject) item).get(answerUser.toUpperCase()).toString());
             }
         });
-        return questionBot;
+        return questionBot.get(owner.id());
     }
 
+    private String greeting(String welcome) {
+        user.setUser(owner);
+        answerUser = null;
+        questionBot.put(user.getId(), welcome);
+
+        return "Olá, " + owner.firstName() + "! " + questionBot.values();
+    }
+
+    private String flowSimple(Object options) {
+        questionBot.put(user.getId(), ((JSONArray) options).get(0).toString());
+        return owner.firstName() + ", " + answerUser + "!!! " + questionBot.get(owner.id());
+    }
+
+    private String notFound() {
+        questionBot.put(user.getId(), "Desculpe, não entendi sua resposta");
+        return questionBot.get(owner.id());
+    }
 }
+
